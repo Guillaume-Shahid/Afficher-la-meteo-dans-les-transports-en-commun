5991f0f46d591ec959d48c2ba99bd3eeed000ae7
Guillaume SHAHID <guillaume.shahid@gmail.com>
2023-12-07 15:33:20 +0100

    Connect Qpi with html

--- public/js/meteo.js
+++ public/js/meteo.js
@@ -1,9 +1,14 @@
 fetch('/weather')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        document.getElementById('temperature').textContent = data.current.temp_c + '°C';
+        document.getElementById('ville').textContent = data.location.name;
+        document.getElementById('condition').textContent = data.current.condition.text;
+        document.getElementById('icon').src = data.current.condition.icon;
+        document.getElementById('humidity').textContent = data.current.humidity + '%';
+        document.getElementById('windkm').textContent = data.current.wind_kph + 'km/h';
     })
     .catch(error => {
         console.error('Erreur:', error);
-        document.getElementById('').textContent = 'Erreur lors de la récupération des données';
+        document.getElementById('weatherData').textContent = 'Erreur lors de la récupération des données';
     });
