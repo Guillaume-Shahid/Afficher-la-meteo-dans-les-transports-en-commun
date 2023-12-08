ed8c93a2d3fce6316675d09f0489c3f357bdefff
Guillaume SHAHID <guillaume.shahid@gmail.com>
2023-12-08 08:24:41 +0100

    Add last update and improve UI

--- public/js/meteo.js
+++ public/js/meteo.js
@@ -1,14 +1,21 @@
-fetch('/weather')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('temperature').textContent = data.current.temp_c + '°C';
-        document.getElementById('ville').textContent = data.location.name;
-        document.getElementById('condition').textContent = data.current.condition.text;
-        document.getElementById('icon').src = data.current.condition.icon;
-        document.getElementById('humidity').textContent = data.current.humidity + '%';
-        document.getElementById('windkm').textContent = data.current.wind_kph + 'km/h';
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-        document.getElementById('weatherData').textContent = 'Erreur lors de la récupération des données';
-    });
+function fetchWeatherData() {
+    fetch('/weather')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('temperature').textContent = data.current.temp_c + '°C';
+            document.getElementById('ville').textContent = data.location.name;
+            document.getElementById('condition').textContent = data.current.condition.text;
+            document.getElementById('icon').src = data.current.condition.icon;
+            document.getElementById('humidity').textContent = data.current.humidity + '%';
+            document.getElementById('windkm').textContent = data.current.wind_kph + 'km/h';
+            document.getElementById('hour').textContent = 'last update : ' + new Date().toLocaleTimeString();
+        })
+        .catch(error => {
+            console.error('Erreur:', error);
+            document.getElementById('weatherData').textContent = 'Erreur lors de la récupération des données';
+        });
+}
+
+fetchWeatherData();
+
+setInterval(fetchWeatherData, 60 * 60 * 1000);
